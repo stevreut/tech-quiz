2a0471804fe16f9218d17d8d5675ea4309bf0739
Stephen Reuterskiold <steve.reuterskiold@gmail.com>
2023-07-29 07:23:46 -0400

    timer implementation in script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,4 +14,45 @@ function consoleLogQandA () {
     }
 }
 
-consoleLogQandA();
+let startButton = document.querySelector("#start-btn");
+let clockPara = document.querySelector("#clock");
+let secondsRemaining = -1;  // initial place-holder value
+let timerID = -1;
+
+function disableStartButton() {
+    startButton.setAttribute("style","visibility:hidden;");  // TODO - is this best?  Should we disable instead?
+}
+
+function showTime() {
+    clockPara.textContent = (secondsRemaining + " seconds left");
+}
+
+function startTimer() {
+    clockPara.setAttribute("style","visibility:visible;");
+    secondsRemaining = 100;  // TODO
+    showTime();
+    timerID = setInterval(function() {
+        secondsRemaining--;
+        if (secondsRemaining <= 0) {
+            clearInterval(timerID);
+            // TODO - other?
+        } else {
+            showTime();
+        }
+    }, 1000);
+}
+
+function startQuiz() {
+    disableStartButton();
+    startTimer();
+    loopThruQuestions();
+    showResult();
+}
+
+function init() {
+    startButton.addEventListener("click",startQuiz);    
+}
+
+consoleLogQandA();  // TODO - comment out eventually
+
+init();
